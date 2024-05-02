4f7fa1698393777896a078c7b2273592eb2a94d6
timharrison-moj <47779388+timharrison-moj@users.noreply.github.com>
2024-05-02 12:36:07 +0100

    BAVL-73: Restrict access to ROLE_VIDEO_LINK_COURT_USER (#6)

--- server/app.ts
+++ server/app.ts
@@ -19,6 +19,7 @@ import setUpWebSession from './middleware/setUpWebSession'
 
 import routes from './routes'
 import type { Services } from './services'
+import AuthorisedRoles from './enumeration/authorisedRoles'
 
 export default function createApp(services: Services): express.Application {
   const app = express()
@@ -36,7 +37,7 @@ export default function createApp(services: Services): express.Application {
   app.use(setUpStaticResources())
   nunjucksSetup(app, services.applicationInfo)
   app.use(setUpAuthentication())
-  app.use(authorisationMiddleware())
+  app.use(authorisationMiddleware([AuthorisedRoles.VIDEO_LINK_COURT_USER]))
   app.use(setUpCsrf())
   app.use(setUpCurrentUser(services))
 

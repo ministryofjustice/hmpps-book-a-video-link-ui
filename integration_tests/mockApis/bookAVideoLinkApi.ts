ab7a601a2afbd213430d9c31063f9315f75f8d23
Stephen Mc Veigh <smcveigh941@gmail.com>
2024-06-11 14:07:33 +0100

    cypress

--- integration_tests/mockApis/bookAVideoLinkApi.ts
+++ integration_tests/mockApis/bookAVideoLinkApi.ts
@@ -11,6 +11,63 @@ const ping = () =>
     },
   })
 
+const stubUserCourtPreferences = () =>
+  stubFor({
+    request: {
+      method: 'GET',
+      urlPattern: '/book-a-video-link-api/courts/user-preferences',
+    },
+    response: {
+      status: 200,
+      headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+      jsonBody: [
+        {
+          courtId: 304,
+          code: 'ABERCV',
+          description: 'Aberystwyth Civil',
+          enabled: true,
+          notes: null,
+        },
+        {
+          courtId: 305,
+          code: 'ABERFC',
+          description: 'Aberystwyth Family',
+          enabled: true,
+          notes: null,
+        },
+      ],
+    },
+  })
+
+const stubUserProbationTeamPreferences = () =>
+  stubFor({
+    request: {
+      method: 'GET',
+      urlPattern: '/book-a-video-link-api/probation-teams/user-preferences',
+    },
+    response: {
+      status: 200,
+      headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+      jsonBody: [
+        {
+          probationTeamId: 1,
+          code: 'BLKPPP',
+          description: 'Blackpool MC (PPOC)',
+          enabled: true,
+          notes: null,
+        },
+        {
+          probationTeamId: 8,
+          code: 'BURNPM',
+          description: 'Burnley MC (PPOC)',
+          enabled: true,
+          notes: null,
+        },
+      ],
+    },
+  })
+
 export default {
   stubBookAVideoLinkPing: ping,
+  stubUserPreferences: () => Promise.all([stubUserCourtPreferences(), stubUserProbationTeamPreferences()]),
 }

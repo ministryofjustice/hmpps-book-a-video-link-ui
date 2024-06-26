a64284e3777a8d9c720009d844d60805599de62d
Stephen McVeigh <smcveigh941@gmail.com>
2024-06-26 09:58:34 +0100

    BAVL-226: Add form spinner component to form submissions (#45)

--- frontend/all.js
+++ frontend/all.js
@@ -1,6 +1,7 @@
 import { nodeListForEach } from './utils'
 import Card from './components/card/card'
 import DatePicker from './components/hmpps-date-picker/datePicker'
+import FormSpinner from './components/form-spinner/form-spinner'
 
 function initAll() {
   const $cards = document.querySelectorAll('.card--clickable')
@@ -11,6 +12,11 @@ function initAll() {
   const $datePickers = document.querySelectorAll('[data-module="hmpps-datepicker"]')
   nodeListForEach($datePickers, function ($datePicker) {
     new DatePicker($datePicker, {}).init()
+  })
+
+  var $spinnerForms = document.querySelectorAll('[data-module="form-spinner"]')
+  nodeListForEach($spinnerForms, function ($spinnerForm) {
+    new FormSpinner($spinnerForm)
   })
 }
 

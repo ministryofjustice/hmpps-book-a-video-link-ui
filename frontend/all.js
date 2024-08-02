9ab3c83d4f8d122d904b2369e6865a45a3e359d8
Stephen McVeigh <smcveigh941@gmail.com>
2024-08-02 09:49:03 +0100

    BAVL-281: Use the date picker from the new MoJ Components release (#59)

--- frontend/all.js
+++ frontend/all.js
@@ -1,17 +1,11 @@
 import { nodeListForEach } from './utils'
 import Card from './components/card/card'
-import DatePicker from './components/hmpps-date-picker/datePicker'
 import FormSpinner from './components/form-spinner/form-spinner'
 
 function initAll() {
   const $cards = document.querySelectorAll('.card--clickable')
   nodeListForEach($cards, function ($card) {
     new Card($card)
-  })
-
-  const $datePickers = document.querySelectorAll('[data-module="hmpps-datepicker"]')
-  nodeListForEach($datePickers, function ($datePicker) {
-    new DatePicker($datePicker, {}).init()
   })
 
   var $spinnerForms = document.querySelectorAll('[data-module="form-spinner"]')

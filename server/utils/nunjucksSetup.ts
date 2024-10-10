490ebda5d0ed9a3649120dd64931efec38ee2371
Stephen McVeigh <smcveigh941@gmail.com>
2024-10-10 10:39:13 +0100

    Add feedback link

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -21,6 +21,7 @@ export default function nunjucksSetup(app: express.Express, applicationInfo: App
   app.locals.authUrl = config.apis.hmppsAuth.externalUrl
   app.locals.environmentName = config.environmentName
   app.locals.environmentNameColour = config.environmentName === 'PRE-PRODUCTION' ? 'govuk-tag--green' : ''
+  app.locals.feedbackUrl = config.feedbackUrl
 
   app.use((req, res, next) => {
     res.locals.session = req.session

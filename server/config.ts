8b8ba0072de5ab802c1cec7f187bf1fef9b3bd40
Stephen McVeigh <smcveigh941@gmail.com>
2024-10-10 11:26:54 +0100

    Fix deployment of feedback url

--- server/config.ts
+++ server/config.ts
@@ -120,7 +120,7 @@ export default {
   },
   domain: get('INGRESS_URL', 'http://localhost:3000', requiredInProduction),
   environmentName: get('ENVIRONMENT_NAME', ''),
-  feedbackUrl: get('FEEDBACK_URL', '#', requiredInProduction),
+  feedbackUrl: get('FEEDBACK_URL', '#'),
   maintenance: {
     enabled: get('MAINTENANCE_MODE', 'false') === 'true',
     startDateTime: get('MAINTENANCE_START_DATETIME', null),

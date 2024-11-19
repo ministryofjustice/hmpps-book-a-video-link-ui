3d053a389016823b5cdb8ebdc3cc915b606bfe6a
Stephen McVeigh <smcveigh941@gmail.com>
2024-11-19 10:25:00 +0000

    Upgrade Cheerio

--- server/routes/journeys/bookAVideoLink/handlers/checkBookingHandler.test.ts
+++ server/routes/journeys/bookAVideoLink/handlers/checkBookingHandler.test.ts
@@ -1,6 +1,6 @@
 import type { Express } from 'express'
 import request from 'supertest'
-import cheerio from 'cheerio'
+import * as cheerio from 'cheerio'
 import { appWithAllRoutes, journeyId, user } from '../../../testutils/appSetup'
 import AuditService, { Page } from '../../../../services/auditService'
 import { existsByDataQa, getPageHeader } from '../../../testutils/cheerio'

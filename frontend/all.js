0c04ba758f051bb35a7daa202c3cdb3bade90b5c
Stephen McVeigh <smcveigh941@gmail.com>
2024-11-27 10:42:16 +0000

    Add back links

--- frontend/all.js
+++ frontend/all.js
@@ -1,8 +1,14 @@
 import { nodeListForEach } from './utils'
+import BackLink from './components/back-link/back-link'
 import Card from './components/card/card'
 import FormSpinner from './components/form-spinner/form-spinner'
 
 function initAll() {
+  var $backLinks = document.querySelectorAll('.govuk-back-link')
+  nodeListForEach($backLinks, function ($backLink) {
+    new BackLink($backLink)
+  })
+
   const $cards = document.querySelectorAll('.card--clickable')
   nodeListForEach($cards, function ($card) {
     new Card($card)

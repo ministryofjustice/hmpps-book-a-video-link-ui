9d1c1aa77234aa399714f210b823f20b4a097cec
Stephen McVeigh <smcveigh941@gmail.com>
2025-03-06 12:40:25 +0000

    BAVL-654: Prepopulate add another component (#174)

--- frontend/components/moj-add-another/moj-add-another.js
+++ frontend/components/moj-add-another/moj-add-another.js
@@ -1,8 +1,18 @@
 function MojAddAnother($component) {
-  MOJFrontend.AddAnother.prototype.createRemoveButton = function (item) {
-    item.append(
-      '<button type="button" class="moj-add-another__remove-button">Remove</button>'
-    );
+  // Our component does not have a heading, so we override this function to do nothing
+  MOJFrontend.AddAnother.prototype.focusHeading = () => {}
+
+  // Our component's remove button has different classes to the original, so we override this function
+  MOJFrontend.AddAnother.prototype.createRemoveButton = (item) => item.append('<button type="button" class="moj-add-another__remove-button">Remove</button>');
+
+  // The original component clones the first element when clicking "Add another", including the value of that element, which we want to leave blank
+  MOJFrontend.AddAnother.prototype.getNewItem = function () {
+    const item = this.getItems().first().clone();
+    if (!this.hasRemoveButton(item)) {
+      this.createRemoveButton(item);
+    }
+    item.find('input, textarea, select').val('')
+    return item
   };
 
   new MOJFrontend.AddAnother($component)

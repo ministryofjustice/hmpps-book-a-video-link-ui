1c1a35fdba58550e60afeaddbbb458b229abf5cf
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2025-04-28 09:18:18 +0100

    BAVL-726 adding support for probation team appointment CSV download from the view bookings screen. (#208)

--- server/routes/journeys/viewBooking/handlers/downloadCsvHandler.test.ts
+++ server/routes/journeys/viewBooking/handlers/downloadCsvHandler.test.ts
@@ -49,7 +49,7 @@ afterEach(() => {
 describe('GET', () => {
   it('should download court csv for today', () => {
     const expectedCsv =
-      'Appointment Start Time,Appointment End Time,Prison,Prisoner name,Prisoner Number,Hearing Type,Court Hearing Link,Room Hearing Link\n12:45,13:15,HMP Moorland,John Doe,A1234AA,APPEAL,https://video.link.url,'
+      'Appointment Start Time,Appointment End Time,Prison,Prisoner Name,Prisoner Number,Hearing Type,Court Hearing Link,Room Hearing Link\n12:45,13:15,HMP Moorland,John Doe,A1234AA,Appeal hearing,https://court.link.url,'
 
     videoLinkService.getVideoLinkSchedule.mockResolvedValue([getCourtBooking()])
 
@@ -76,6 +76,36 @@ describe('GET', () => {
         )
       })
   })
+
+  it('should download probation team csv for today', () => {
+    const expectedCsv =
+      'Appointment Start Time,Appointment End Time,Prison,Prisoner Name,Prisoner Number,Meeting Type,Room Hearing Link,Probation Officer Name,Email Address\n12:45,13:15,HMP Moorland,Jane Doe,A1234AA,Pre sentence report,https://room.link.url,Jane Doe,jane.doe@email.com'
+
+    videoLinkService.getVideoLinkSchedule.mockResolvedValue([getProbationTeamBooking()])
+
+    return request(app)
+      .get('/probation/view-booking/download-csv')
+      .expect('Content-Type', /text\/csv; charset=utf-8/)
+      .expect(
+        'Content-Disposition',
+        `attachment; filename="VideoLinkBookings-${formatDate(new Date(), 'yyyy-MM-dd')}-Probation_1.csv"`,
+      )
+      .expect(res => {
+        expect(auditService.logPageView).toHaveBeenCalledWith(Page.DOWNLOAD_BOOKINGS_PAGE, {
+          who: user.username,
+          correlationId: expect.any(String),
+        })
+
+        expect(res.text).toEqual(expectedCsv)
+
+        expect(videoLinkService.getVideoLinkSchedule).toHaveBeenLastCalledWith(
+          'probation',
+          'P1',
+          startOfDay(new Date()),
+          user,
+        )
+      })
+  })
 })
 
 const getCourtBooking = (): ScheduleItem & { prisonerName: string; prisonLocationDescription: string } => ({
@@ -83,8 +113,7 @@ const getCourtBooking = (): ScheduleItem & { prisonerName: string; prisonLocatio
   prisonAppointmentId: 1,
   bookingType: 'COURT',
   statusCode: 'ACTIVE',
-  videoUrl: 'https://video.link.url',
-  bookingComments: 'Free text comment',
+  videoUrl: 'https://court.link.url',
   createdByPrison: 'false',
   courtId: 1,
   courtCode: 'C1',
@@ -96,7 +125,6 @@ const getCourtBooking = (): ScheduleItem & { prisonerName: string; prisonLocatio
   prisonerNumber: 'A1234AA',
   appointmentType: 'VLB_COURT_MAIN',
   appointmentTypeDescription: 'Court - main hearing',
-  appointmentComments: 'This is a free text comment',
   prisonLocKey: 'MDI-VCC-1',
   prisonLocDesc: 'VCC-crown-conference-room-1',
   dpsLocationId: 'a4fe3fef-34fd-4354fde-a12efe',
@@ -105,7 +133,37 @@ const getCourtBooking = (): ScheduleItem & { prisonerName: string; prisonLocatio
   endTime: '13:15',
   createdTime: '2024-10-01 14:45',
   createdBy: 'creator@email.com',
-  updatedTime: '2024-10-02 14:45',
   prisonerName: 'john doe',
   prisonLocationDescription: 'Location description',
+})
+
+const getProbationTeamBooking = (): ScheduleItem & { prisonerName: string; prisonLocationDescription: string } => ({
+  videoBookingId: 2,
+  prisonAppointmentId: 2,
+  bookingType: 'PROBATION',
+  statusCode: 'ACTIVE',
+  videoUrl: 'https://room.link.url',
+  createdByPrison: 'false',
+  probationTeamId: 1,
+  probationTeamCode: 'P1',
+  probationTeamDescription: 'Probation 1',
+  probationMeetingType: 'PSR',
+  probationMeetingTypeDescription: 'Pre sentence report',
+  prisonCode: 'MDI',
+  prisonName: 'HMP Moorland',
+  prisonerNumber: 'A1234AA',
+  appointmentType: 'VLB_PROBATION',
+  appointmentTypeDescription: 'Probation meeting',
+  prisonLocKey: 'MDI-VCC-1',
+  prisonLocDesc: 'VCC-crown-conference-room-1',
+  dpsLocationId: 'a4fe3fef-34fd-4354fde-a12efe',
+  appointmentDate: '2024-10-03',
+  startTime: '12:45',
+  endTime: '13:15',
+  createdTime: '2024-10-01 14:45',
+  createdBy: 'creator@email.com',
+  prisonerName: 'jane doe',
+  prisonLocationDescription: 'Location description',
+  probationOfficerName: 'Jane Doe',
+  probationOfficerEmailAddress: 'jane.doe@email.com',
 })

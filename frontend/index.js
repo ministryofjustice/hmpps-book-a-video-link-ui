01c87cc3d5f6e2cc3e65e1d30195d791395cb69a
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2025-11-05 14:14:35 +0000

    BAVL-1141 UI patch for MOJ frontend.

--- frontend/index.js
+++ frontend/index.js
@@ -1,11 +1,11 @@
-import jQuery from 'jquery'
+import $ from 'jquery'
 import * as GOVUKFrontend from 'govuk-frontend'
 import * as MOJFrontend from '@ministryofjustice/frontend'
 import * as BookAVideoLinkFrontend from './all'
 
 // JQuery required by MoJ frontend.
 // https://design-patterns.service.justice.gov.uk/get-started/setting-up-javascript/
-window.jQuery = jQuery
+window.$ = $ // JQuery required by MoJ frontend. https://design-patterns.service.justice.gov.uk/get-started/setting-up-javascript/
 
 // Make GOVUKFrontend And MOJFrontend globally accessible
 window.GOVUKFrontend = GOVUKFrontend

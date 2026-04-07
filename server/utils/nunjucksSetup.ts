a9147138360c81a4bb7cd7551469a274b7cdc685
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2026-04-07 13:33:09 +0100

    BAVL-1283 removing the feature toggle for temporary room blocking. Temporary room blocking is now available by default.

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -96,5 +96,4 @@ export default function nunjucksSetup(app: express.Express, applicationInfo: App
 
   // Default base URLs for court meeting links
   njkEnv.addGlobal('defaultCourtVideoUrl', config.defaultCourtVideoUrl)
-  njkEnv.addGlobal('temporaryBlockingLocations', config.featureToggles.temporaryBlockingLocations)
 }

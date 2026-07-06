309f944d4ac9623e3572d384fa6d0e50a7a15683
Tim Harrison <tim.harrison@digital.justice.gov.uk>
2026-07-06 12:20:38 +0100

    BAVL-1397: Recording a log message to identify delius-specific auth issues

--- server/middleware/authorisationMiddleware.test.ts
+++ server/middleware/authorisationMiddleware.test.ts
@@ -3,11 +3,11 @@ import type { Request, Response } from 'express'
 
 import authorisationMiddleware from './authorisationMiddleware'
 
-function createToken(authorities: string[]) {
+function createToken(authorities: string[], authSource: string) {
   const payload = {
     user_name: 'USER1',
     scope: ['read', 'write'],
-    auth_source: 'nomis',
+    auth_source: authSource,
     authorities,
     jti: 'a610a10-cca6-41db-985f-e87efb303aaf',
     client_id: 'clientid',
@@ -20,11 +20,11 @@ describe('authorisationMiddleware', () => {
   let req: Request
   const next = jest.fn()
 
-  function createResWithToken({ authorities }: { authorities: string[] }): Response {
+  function createResWithToken({ authorities, authSource }: { authorities: string[]; authSource: string }): Response {
     return {
       locals: {
         user: {
-          token: createToken(authorities),
+          token: createToken(authorities, authSource),
         },
       },
       redirect: jest.fn(),
@@ -36,7 +36,7 @@ describe('authorisationMiddleware', () => {
   })
 
   it('should return next when no required roles', () => {
-    const res = createResWithToken({ authorities: [] })
+    const res = createResWithToken({ authorities: [], authSource: 'nomis' })
 
     authorisationMiddleware()(req, res, next)
 
@@ -45,7 +45,7 @@ describe('authorisationMiddleware', () => {
   })
 
   it('should redirect when user has no authorised roles', () => {
-    const res = createResWithToken({ authorities: [] })
+    const res = createResWithToken({ authorities: [], authSource: 'nomis' })
 
     authorisationMiddleware(['SOME_REQUIRED_ROLE'])(req, res, next)
 
@@ -54,7 +54,7 @@ describe('authorisationMiddleware', () => {
   })
 
   it('should return next when user has authorised role', () => {
-    const res = createResWithToken({ authorities: ['ROLE_SOME_REQUIRED_ROLE'] })
+    const res = createResWithToken({ authorities: ['ROLE_SOME_REQUIRED_ROLE'], authSource: 'nomis' })
 
     authorisationMiddleware(['SOME_REQUIRED_ROLE'])(req, res, next)
 
@@ -63,7 +63,25 @@ describe('authorisationMiddleware', () => {
   })
 
   it('should return next when user has authorised role and middleware created with ROLE_ prefix', () => {
-    const res = createResWithToken({ authorities: ['ROLE_SOME_REQUIRED_ROLE'] })
+    const res = createResWithToken({ authorities: ['ROLE_SOME_REQUIRED_ROLE'], authSource: 'nomis' })
+
+    authorisationMiddleware(['ROLE_SOME_REQUIRED_ROLE'])(req, res, next)
+
+    expect(next).toHaveBeenCalled()
+    expect(res.redirect).not.toHaveBeenCalled()
+  })
+
+  it('should redirect for nDelius users without a required role', () => {
+    const res = createResWithToken({ authorities: ['ROLE_SOME_DELIUS_ROLE'], authSource: 'delius' })
+
+    authorisationMiddleware(['ROLE_SOME_REQUIRED_ROLE'])(req, res, next)
+
+    expect(next).not.toHaveBeenCalled()
+    expect(res.redirect).toHaveBeenCalled()
+  })
+
+  it('should return next when user has authorised role for nDelius users', () => {
+    const res = createResWithToken({ authorities: ['ROLE_SOME_REQUIRED_ROLE'], authSource: 'delius' })
 
     authorisationMiddleware(['ROLE_SOME_REQUIRED_ROLE'])(req, res, next)
 
--- server/middleware/authorisationMiddleware.ts
+++ server/middleware/authorisationMiddleware.ts
@@ -5,21 +5,34 @@ import logger from '../../logger'
 
 export default function authorisationMiddleware(authorisedRoles: string[] = []): RequestHandler {
   return (req, res, next) => {
-    // authorities in the user token will always be prefixed by ROLE_.
-    // Convert roles that are passed into this function without the prefix so that we match correctly.
+    // Authorities in the user token will always be prefixed by ROLE_.
+    // Convert any roles passed into this function without a ROLE_ prefix so that we match like for like.
     const authorisedAuthorities = authorisedRoles.map(role => (role.startsWith('ROLE_') ? role : `ROLE_${role}`))
     if (res.locals?.user?.token) {
-      const { authorities: roles = [] } = jwtDecode(res.locals.user.token) as { authorities?: string[] }
+      const { authorities: roles = [], auth_source: source = '' } = jwtDecode(res.locals.user.token) as {
+        authorities?: string[]
+        auth_source?: string
+      }
 
+      // Check for an allowed role
       if (authorisedAuthorities.length && !roles.some(role => authorisedAuthorities.includes(role))) {
+        // Separately identify nDelius users from their auth source and record a log event we can report upon
+        if (source === 'delius') {
+          logger.error(`Delius user is missing any BVLS roles`)
+        }
+
+        // Redirect to the authorisation error page
         logger.error('User is not authorised to access this')
         return res.redirect('/authError')
       }
 
+      // Authorised users
       return next()
     }
 
+    // User is not logged in / has no token in their session
     req.session.returnTo = req.originalUrl
+
     return res.redirect('/sign-in')
   }
 }

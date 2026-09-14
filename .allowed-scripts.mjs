72aeb96ffd263f1e2ca7298bdc357a609c10aa3a
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2026-09-14 11:16:47 +0100

    BAVL-1471 dependency updates for security patch.

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -3,14 +3,12 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 export default configureAllowedScripts({
   allowlist: {
     // Needed by esbuild for watching files during development
-    'node_modules/@parcel/watcher@2.5.0': 'ALLOW',
+    'node_modules/@parcel/watcher@2.6.0': 'ALLOW',
     'node_modules/esbuild@0.28.2': 'ALLOW',
     // Provides native integration, supporting the ability to write dtrace probes for bunyan
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
     // Native solution to quickly resolve module paths, used by jest and eslint
-    'node_modules/unrs-resolver@1.11.1': 'ALLOW',
-    // Needed by playwright for detecting file system changes during test runs
-    'node_modules/playwright/node_modules/fsevents@2.3.2': 'ALLOW',
+    'node_modules/unrs-resolver@1.12.2': 'ALLOW',
     'node_modules/fsevents@2.3.3': 'ALLOW',
   },
 })
